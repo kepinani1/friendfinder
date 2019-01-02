223ad7306771b10e6805ca2ae73e45d392836845
Ipek Inan <kepinani1@gmail.com>
2019-01-02 13:17:59 -0500

    final

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -12,39 +12,35 @@ module.exports = function (app) {
     // Set the post for the api/friends route
     app.post('/api/friends', function (req, res) {
         //setting of applicable variables
-        var difference = 30;
-        var name = '';
-        var photo = '';
 
-        // Looping through friends.js to find a match
-        friends.forEach(function (friend) {
-            // Variables for comparing matches (an array comparing user scores, and another for the total difference). The total difference of 30 is just a number I made up. I thought it would be a reasonable number for max difference.
-            var userScores = [];
-            var totalDifference = [];
+    var otherFriendScore = req.body.scores;
+    var scoresArray = [];
+    var userMatch = 0;
 
-            //Loops through users scores, and compares absolute difference from friend array
-            for (var i = 0; i < friend.scores.length; i++) {
-                userScores.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(friend.scores[i])));
-
-            }
-
-
-            // If the above value is smaller than the previous difference...
-            if (totalDifference < difference) {
-                // Set it as the previous difference...
-                difference = totalDifference;
-                // And set these variables to the appropriate celeb match
-                name = friend.name;
-                photo = friend.photo;
-            }
-        });
-        //match with least difference will appear. This will be interpreted as the celeb the user has the highest compatibility with.
-        res.json({
-            name: name,
-            photo: photo
-        });
-
-        // This adds the new users sent data object to friends.js
-        friends.push(req.body);
+     //runs through all current friends in list
+     for(var i=0; i<friends.length; i++){
+        var scoresDifference = 0;
+        //run through scores to compare friends
+        for(var j=0; j<otherFriendScore.length; j++){
+          scoresDifference += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(otherFriendScore[j])));
+        }
+  
+        //push results into scoresArray
+        scoresArray.push(scoresDifference);
+      }
+  
+      //after all friends are compared, find best match
+      for(var i=0; i<scoresArray.length; i++){
+        if(scoresArray[i] <= scoresArray[userMatch]){
+          userMatch = i;
+        }
+      }
+  
+      //return bestMatch data
+      var idealMate = friends[userMatch];
+      res.json(idealMate);
+  
+      //pushes new submission into the friendsList array
+      friends.push(req.body);
     });
-}
+  };
